28961cc45f27edfb99194c706040668beb184496
jessbuenos@gmail.com <Brussel.20>
2021-07-19 13:06:17 +0200

    regressor with predict function

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ import pandas as pd
 from utils.DataCleaner import DataCleaner
 from utils.DataRegressor import DataRegressor
 
+pd.set_option('display.max_columns', None)
+
 data = pd.read_csv('assets/housing-data.csv', index_col=0)
 
 dataAnalyser = DataCleaner(data)
 dataAnalyser.clean()
 
 dataRegressor = DataRegressor(dataAnalyser.df)
-dataRegressor.predict()
+dataRegressor.trainModel()
+
+data = pd.read_csv('assets/housing-data.csv', index_col=0).head(1000)
+
+dataAnalyser = DataCleaner(data)
+dataAnalyser.clean()
+
+predictions = dataRegressor.predict(dataAnalyser.df)
+print('######## data to predict ############')
+print(dataAnalyser.df.head().price**4)
+print('#########" predictions ###########"')
+print(predictions)
